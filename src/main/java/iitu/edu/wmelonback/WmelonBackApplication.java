fb9a59cc0e0ed38396a9df49f19bcac0d6301f3c
Ulan <kozhabekov.u@gmail.com>
2022-09-05 23:48:08 +0600

    Spring Initializr

---
+++ src/main/java/iitu/edu/wmelonback/WmelonBackApplication.java
@@ -0,0 +1,13 @@
+package iitu.edu.wmelonback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WmelonBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WmelonBackApplication.class, args);
+    }
+
+}
---
+++ src/test/java/iitu/edu/wmelonback/WmelonBackApplicationTests.java
@@ -0,0 +1,13 @@
+package iitu.edu.wmelonback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WmelonBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
